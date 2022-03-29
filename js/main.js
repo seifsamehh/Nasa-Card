617c5b9a93e00f653d9ee36a25bef455ab9e8f41
seifsamehh <seifsameh1432@gmail.com>
2022-03-29 18:39:56 +0200

    Nasa Card

---
+++ js/main.js
@@ -0,0 +1,111 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("left"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "https://assets5.lottiefiles.com/packages/lf20_tno6cg2w.json", // lottie file path
+});
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("leftt"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "https://assets1.lottiefiles.com/private_files/lf30_zSGy1w.json", // lottie file path
+});
+// target elements with the "draggable" class
+interact(".draggable").draggable({
+  // enable inertial throwing
+  inertia: true,
+  // keep the element within the area of it's parent
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: "parent",
+      endOnly: true,
+    }),
+  ],
+  // enable autoScroll
+  autoScroll: true,
+
+  listeners: {
+    // call this function on every dragmove event
+    move: dragMoveListener,
+
+    // call this function on every dragend event
+    end(event) {
+      var textEl = event.target.querySelector("p");
+
+      textEl &&
+        (textEl.textContent =
+          "moved a distance of " +
+          Math.sqrt(
+            (Math.pow(event.pageX - event.x0, 2) +
+              Math.pow(event.pageY - event.y0, 2)) |
+              0
+          ).toFixed(2) +
+          "px");
+    },
+  },
+});
+
+function dragMoveListener(event) {
+  var target = event.target;
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+  // translate the element
+  target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+  // update the posiion attributes
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
+// for developed by
+var textWrapper = document.querySelector(".ml1 .letters");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml1 .letter",
+    scale: [0.3, 1],
+    opacity: [0, 1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i + 1),
+  })
+  .add({
+    targets: ".ml1 .line",
+    scaleX: [0, 1],
+    opacity: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: "-=875",
+    delay: (el, i, l) => 80 * (l - i),
+  })
+  .add({
+    targets: ".ml1",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
+  // for scroll
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    multiplier: 0.5,
+    touchMultiplier: 1,
+    smartphone: {
+      smooth: false
+    },
+    tablet: {
+      smooth: false
+    }
+});
